6804b5f7ee5ffa63bf06606ace2083248b71a746
innovatorCho <spectre1c@gmail.com>
2021-10-29 23:58:47 +0900

    todo list loading

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,18 @@ const toDoInput = toDoForm.querySelector("input");
 
 const toDoList = document.querySelector(".todo-list");
 
+const TODO_KEY = "todos";
+let toDos = [];
+
+saveToDos = () => {
+    localStorage.setItem(TODO_KEY, JSON.stringify(toDos));
+};
+
 deleteToDo = (event) => {
     event.preventDefault();
     const item = event.target.parentElement;
     item.remove();
-}
+};
 
 paintToDo = (newTodo) => {
     const li = document.createElement("li");
@@ -21,13 +28,26 @@ paintToDo = (newTodo) => {
     button.innerText = " X ";
     button.addEventListener("click", deleteToDo);
     toDoList.appendChild(li);
-}
+};
 
 function handleToDoForm(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoForm);
+
+const savedToDos = localStorage.getItem(TODO_KEY);
+
+if(savedToDos !== null) {
+    const todoItems = JSON.parse(savedToDos);
+    toDos = todoItems;
+    todoItems.forEach(paintToDo);
+}
+
+

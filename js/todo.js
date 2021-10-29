a5ff65007694dc94f3b413a59b98024047629875
innovatorCho <spectre1c@gmail.com>
2021-10-29 23:20:15 +0900

    todo list item delete

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,23 @@ const toDoInput = toDoForm.querySelector("input");
 
 const toDoList = document.querySelector(".todo-list");
 
+deleteToDo = (event) => {
+    event.preventDefault();
+    const item = event.target.parentElement;
+    item.remove();
+}
+
 paintToDo = (newTodo) => {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+
+    const button = document.createElement("button");
+    li.appendChild(span);
+    li.appendChild(button);
+
+    button.innerText = " X ";
+    button.addEventListener("click", deleteToDo);
     toDoList.appendChild(li);
 }
 
